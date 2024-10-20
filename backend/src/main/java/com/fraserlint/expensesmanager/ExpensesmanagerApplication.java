eed1b95bfb2f3385edbcec165d59add1400fe322
Austin Davison <davisonaustin287@gmail.com>
2024-10-20 20:58:27 +0100

    intitialised backend

---
+++ backend/src/main/java/com/fraserlint/expensesmanager/ExpensesmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fraserlint.expensesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesmanagerApplication.class, args);
+	}
+
+}
